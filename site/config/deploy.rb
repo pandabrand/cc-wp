946c2bc29515451f7f434c8b01584fd1aaf2e096
pandabrand <pandabrand@gmail.com>
2017-07-26 21:20:56 -0400

    moving to current_path

--- site/config/deploy.rb
+++ site/config/deploy.rb
@@ -32,8 +32,8 @@ set :gulp_file, -> { release_path.join('web/app/themes/culturecollide-theme/gulp
 set :linked_files, fetch(:linked_files, []).push('.env', 'web/.htaccess')
 set :linked_dirs, fetch(:linked_dirs, []).push('web/app/uploads')
 
-set :composer_install_flags, '--no-interaction --optimize-autoloader --ignore-platform-reqs'
 set :composer_working_dir, -> { current_path.join('site') }
+set :composer_install_flags, '--no-interaction --optimize-autoloader --ignore-platform-reqs'
 
 namespace :deploy do
   desc 'Restart application'
