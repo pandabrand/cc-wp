77eb57ebbbabf2ef75996645e073aee82302f2ed
pandabrand <pandabrand@gmail.com>
2017-09-12 18:47:36 -0500

    favicons addded

--- web/app/themes/culturecollide-theme/templates/head.php
+++ web/app/themes/culturecollide-theme/templates/head.php
@@ -2,5 +2,12 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri() . '/dist/images/favicons/COLLIDE-favicon-96-96.png'; ?>" sizes="16x16">
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri() . '/dist/images/favicons/COLLIDE-favicon-96-96.png'; ?>" sizes="32x32">
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri() . '/dist/images/favicons/COLLIDE-favicon-96-96.png'; ?>" sizes="96x96">
+  <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri() . '/dist/images/favicons/COLLIDE-favicon-120-120.png'; ?>">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() . '/dist/images/favicons/COLLIDE-favicon-180-180.png'; ?>">
+  <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri() . '/dist/images/favicons/COLLIDE-favicon-152-152.png'; ?>">
+  <link rel="apple-touch-icon" sizes="167x167" href="<?php echo get_template_directory_uri() . '/dist/images/favicons/COLLIDE-favicon-167-167.png'; ?>">
   <?php wp_head(); ?>
 </head>
