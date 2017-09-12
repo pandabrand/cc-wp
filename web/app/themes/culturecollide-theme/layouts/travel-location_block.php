edb184a4768bd4e215e4f819458f7050e2ff63b1
pandabrand <pandabrand@gmail.com>
2017-09-12 15:38:59 -0500

    adjusted location cards on map display to handle ariable length content.

--- web/app/themes/culturecollide-theme/layouts/travel-location_block.php
+++ web/app/themes/culturecollide-theme/layouts/travel-location_block.php
@@ -4,7 +4,7 @@
 ?>
 <div class="row travel__detail__map__item <?php $location->ID; ?>" id="<?php echo $location->ID; ?>" data-city="<?php echo $location_city_object->post_name; ?>" data-location-name="<?php echo $location->post_name; ?>">
   <div class="col-12">
-    <div class="card card__2-1">
+    <div class="card card__2-1 card__2-1-travel d-flex flex-column">
         <?php $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $location->ID ), 'large-feature' ); ?>
           <div class="card__image"  style="background-image:<?php echo cc_background_image_filter(); ?>, url('<?php echo esc_url( $large_image_url[0] ); ?>')">
             <div class="card__category-block">
@@ -20,48 +20,48 @@
             </div>
             <div class="card__category-line"></div>
             <div class="card__filter"></div>
-            <div class="card__body">
-              <div class="card__copy d-flex flex-column">
-                <div class="card__title">
-                  <?php echo get_card_title($location); ?>
-                </div>
-                <div class="card__text">
-                  <?php echo $location->post_content; ?>
-                </div>
-                <div class="card__address">
-                  <?php $address = get_field('address', $location->ID); ?>
-                  <?php echo $address['address']; ?>
-                </div>
-                <?php
-                  $loc_id = $location->ID;
-                  global $wpdb;
-                  $artist_posts = $wpdb->get_results( "SELECT * FROM wp_postmeta WHERE meta_key LIKE 'artists_locations_%_location' AND meta_value LIKE '%{$loc_id}%'", OBJECT );
-                  if( !empty( $artist_posts ) ) :
-                ?>
-                  <div class="card_reccomendations">
-                    <div class="medium small_text pt-2">Recommended by</div>
-                    <div class="d-flex flex-row flex-wrap">
-                <?php foreach($artist_posts as $artist): ?>
-                    <div class="card_reccomendations__title small_text pr-2">
-                      <a href="<?= get_the_permalink($artist->post_id) ?>"><?= get_the_title($artist->post_id); ?></a>
-                    </div>
-                <?php endforeach; ?>
-                    </div>
-                  </div>
-                <?php endif; ?>
-                <?php wp_reset_query(); ?>
-                <div class="card__links mt-auto d-flex flex-row justify-content-start">
-                  <div class="card__link">
-                    <a href="<?php echo get_field('website', $location->ID); ?>" rel="external" target="_blank"><i class="fa fa-window-maximize"></i> website</a>
-                  </div>
-                  <div class="card__link">
-                    <a href="https://www.google.com/maps/dir/?api=1&destination=<?php echo $address['lat'],',',$address['lng']; ?>" rel="external" target="_blank">
-                      <i class="fa fa-map"></i> directions
-                    </a>
-                  </div>
-                </div>
-
+          </div><!-- card__image -->
+          <div class="card__body">
+            <div class="card__copy d-flex flex-column">
+              <div class="card__title">
+                <?php echo get_card_title($location); ?>
               </div>
+              <div class="card__text">
+                <?php echo $location->post_content; ?>
+              </div>
+              <div class="card__address">
+                <?php $address = get_field('address', $location->ID); ?>
+                <?php echo $address['address']; ?>
+              </div>
+              <?php
+                $loc_id = $location->ID;
+                global $wpdb;
+                $artist_posts = $wpdb->get_results( "SELECT * FROM wp_postmeta WHERE meta_key LIKE 'artists_locations_%_location' AND meta_value LIKE '%{$loc_id}%'", OBJECT );
+                if( !empty( $artist_posts ) ) :
+              ?>
+                <div class="card_reccomendations">
+                  <div class="medium small_text pt-2">Recommended by</div>
+                  <div class="d-flex flex-row flex-wrap">
+              <?php foreach($artist_posts as $artist): ?>
+                  <div class="card_reccomendations__title small_text pr-2">
+                    <a href="<?= get_the_permalink($artist->post_id) ?>"><?= get_the_title($artist->post_id); ?></a>
+                  </div>
+              <?php endforeach; ?>
+                  </div>
+                </div>
+              <?php endif; ?>
+              <?php wp_reset_query(); ?>
+              <div class="card__links mt-auto d-flex flex-row justify-content-start">
+                <div class="card__link">
+                  <a href="<?php echo get_field('website', $location->ID); ?>" rel="external" target="_blank"><i class="fa fa-window-maximize"></i> website</a>
+                </div>
+                <div class="card__link">
+                  <a href="https://www.google.com/maps/dir/?api=1&destination=<?php echo $address['lat'],',',$address['lng']; ?>" rel="external" target="_blank">
+                    <i class="fa fa-map"></i> directions
+                  </a>
+                </div>
+              </div>
+
             </div>
           </div>
 
--- web/app/themes/culturecollide-theme/taxonomy-location_types.php
+++ web/app/themes/culturecollide-theme/taxonomy-location_types.php
@@ -1,61 +1,67 @@
 <?php get_template_part('templates/content', 'travel_header'); ?>
 <div class="row cc-row travel travel__detail__map__row">
-  <div class="col-6 push-6">
+  <div class="col-sm-12 col-md-6 push-md-6">
     <?php get_template_part('layouts/travel', 'map_block'); ?>
   </div>
-  <div class="col-6 pull-6 travel__detail__map__list">
-    <?php if (!have_posts()) : ?>
-      <div class="alert alert-warning">
-        <?php _e('Sorry, no results were found.', 'sage'); ?>
-      </div>
-      <?php get_search_form(); ?>
-    <?php else: ?>
-      <?php while (have_posts()) : the_post(); ?>
-        <div class="row travel__detail__map__item <?php get_the_id(); ?>" id="<?php echo get_the_id(); ?>">
-          <div class="col-12">
-            <div class="card card__2-1">
-                <?php $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large-feature' ); ?>
-                <div class="card__image"  style="background-image:<?php echo cc_background_image_filter(); ?>, url('<?php echo esc_url( $large_image_url[0] ); ?>')"></div>
-                <div class="card__category-block">
-                  <div class="<?php echo get_post_icon_class(); ?> mr-2"></div>
-                  <div class="card__category-block__category-details pl-3">
-                    <div class="card__category-block__category-type">
-                      <?php echo get_category_type_title();?>
-                    </div>
-                    <div class="card__category-block__category-info">
-                      <?php echo get_category_type_subject(); ?>
-                    </div>
-                  </div>
-                </div>
-                <div class="card__category-line"></div>
-                <div class="card__body">
-                	<div class="card__copy">
-                		<div class="card__title">
-                			<?php echo get_card_title(); ?>
-                		</div>
-                		<div class="card__text">
-                			<?php the_content(); ?>
-                		</div>
-                		<div class="card__address">
-                			<?php $address = get_field('address'); ?>
-                			<?php echo $address['address']; ?>
-                		</div>
-                    <div class="card__links d-flex justify-content-between">
-                      <div class="card__link">
-                        <a href="<?php the_permalink(); ?>" rel="external" target="_blank"><i class="fa fa-desktop"></i> website</a>
+  <div class="col-sm-12 col-md-6 pull-md-6">
+    <div class="travel__detail__map__list--wrapper">
+      <div class="travel__detail__map__list">
+        <?php if (!have_posts()) : ?>
+          <div class="alert alert-warning">
+            <?php _e('Sorry, no results were found.', 'sage'); ?>
+          </div>
+          <?php get_search_form(); ?>
+        <?php else: ?>
+          <?php while (have_posts()) : the_post(); ?>
+            <div class="row travel__detail__map__item <?php get_the_id(); ?>" id="<?php echo get_the_id(); ?>">
+              <div class="col-12">
+                <div class="card card__2-1 card__2-1-travel d-flex flex-column">
+                    <?php $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large-feature' ); ?>
+                    <div class="card__image"  style="background-image:<?php echo cc_background_image_filter(); ?>, url('<?php echo esc_url( $large_image_url[0] ); ?>')">
+                      <div class="card__category-block">
+                        <div class="<?php echo get_post_icon_class(); ?> mr-2"></div>
+                        <div class="card__category-block__category-details pl-3">
+                          <div class="card__category-block__category-type">
+                            <?php echo get_category_type_title();?>
+                          </div>
+                          <div class="card__category-block__category-info">
+                            <?php echo get_category_type_subject(); ?>
+                          </div>
+                        </div>
                       </div>
-                      <div class="card__link">
-                        <a href="https://www.google.com/maps/dir/?api=1&destination=<?php echo $address['lat'],',',$address['lng']; ?>" rel="external" target="_blank">
-                          <i class="fa fa-map"></i> directions
-                        </a>
+                      <div class="card__category-line"></div>
+                      <div class="card__filter"></div>
+                    </div><!-- card__image -->
+                    <div class="card__body">
+                    	<div class="card__copy">
+                    		<div class="card__title">
+                    			<?php echo get_card_title(); ?>
+                    		</div>
+                    		<div class="card__text">
+                    			<?php the_content(); ?>
+                    		</div>
+                    		<div class="card__address">
+                    			<?php $address = get_field('address'); ?>
+                    			<?php echo $address['address']; ?>
+                    		</div>
+                        <div class="card__links d-flex justify-content-between">
+                          <div class="card__link">
+                            <a href="<?php the_permalink(); ?>" rel="external" target="_blank"><i class="fa fa-desktop"></i> website</a>
+                          </div>
+                          <div class="card__link">
+                            <a href="https://www.google.com/maps/dir/?api=1&destination=<?php echo $address['lat'],',',$address['lng']; ?>" rel="external" target="_blank">
+                              <i class="fa fa-map"></i> directions
+                            </a>
+                        	</div>
+                        </div>
                     	</div>
                     </div>
-                	</div>
                 </div>
+              </div>
             </div>
-          </div>
-        </div>
-      <?php endwhile; ?>
-    <?php endif; ?>
+          <?php endwhile; ?>
+        <?php endif; ?>
+      </div>
+    </div>
   </div>
 </div>
