ecbf7a8b5ad5211752b275184d568dbfcfc223df
pandabrand <pandabrand@gmail.com>
2017-09-28 20:58:00 -0500

    changing the related reveal code.

--- web/app/themes/culturecollide-theme/templates/content-editorial-more-content.php
+++ web/app/themes/culturecollide-theme/templates/content-editorial-more-content.php
@@ -40,13 +40,13 @@ if( is_single() && !empty($post_tags) ) {
 
 $more_query = new WP_Query($args);
 if($more_query->have_posts()): $counter = 1; ?>
-<?php if( !is_page( 'editorial' ) ): ?>
-  <div class="row cc-row d-flex justify-content-center">
-    <div class="editorial__title">
+<div class="row cc-row d-flex justify-content-center">
+  <div class="editorial__title">
+    <?php if( !is_page( 'editorial' ) ): ?>
       related
-    </div>
+    <?php endif; ?>
   </div>
-<?php endif; ?>
+</div>
 <div class="row cc-row editorial__block">
   <?php while($more_query->have_posts()): $more_query->the_post(); ?>
     <?php if($counter > 6) { $counter = 1; } ?>
