e55cc0f7fa849cf3d7851d504556e34b307fc3ea
pandabrand <pandabrand@gmail.com>
2017-10-04 11:11:27 -0500

    pushing the iframe wrapper down as well.

--- web/app/themes/culturecollide-theme/lib/culturecollide.php
+++ web/app/themes/culturecollide-theme/lib/culturecollide.php
@@ -352,7 +352,7 @@ function cc_wrap_img_media( $content ) {
   return $content;
 }
 
-add_filter( 'the_content', 'cc_wrap_img_media', 20, 1);
+add_filter( 'the_content', 'cc_wrap_img_media', 20, 1 );
 
 function cc_wrap_iframe( $content ) {
   //
@@ -362,7 +362,7 @@ function cc_wrap_iframe( $content ) {
   return $content;
 }
 
-add_filter( 'the_content', 'cc_wrap_iframe' 20, 1);
+add_filter( 'the_content', 'cc_wrap_iframe' 20, 1 );
 
 add_filter( 'img_caption_shortcode', 'cleaner_caption', 10, 3 );
 
