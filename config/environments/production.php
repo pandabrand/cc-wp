4f0fc425bee38bf5aa8c66e0bb37e1b8baf979d0
pandabrand <pandabrand@gmail.com>
2017-10-27 14:13:20 -0500

    turned on wrong debugging

--- config/environments/production.php
+++ config/environments/production.php
@@ -2,7 +2,9 @@
 /** Production */
 ini_set('display_errors', 0);
 define('WP_DEBUG_DISPLAY', false);
-define('SCRIPT_DEBUG', true);
+define('SCRIPT_DEBUG', false);
+define('WP_DEBUG_LOG', true);
+
 /** Disable all file modifications including updates and update notifications */
 define('DISALLOW_FILE_MODS', true);
 define('GOOGLE_MAP_API','AIzaSyA5pdShpGLoC1YRRNiGiyM_bDAuGwLHcCg');
