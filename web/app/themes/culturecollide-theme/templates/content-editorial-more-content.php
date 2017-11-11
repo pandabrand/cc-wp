77db457806d83eaabbe62524b6b56d6b4c337c79
pandabrand <pandabrand@gmail.com>
2017-11-10 19:07:43 -0600

    changed editorial more block, had a bug where the first post was always missing.

--- web/app/themes/culturecollide-theme/templates/content-editorial-more-content.php
+++ web/app/themes/culturecollide-theme/templates/content-editorial-more-content.php
@@ -52,18 +52,17 @@ if($more_query->have_posts()): $counter = 1; ?>
   </div>
 </div>
 <div class="row cc-row editorial__block">
+  <div class="col-sm-12 col-md-4 push-md-8 col-lg-3 push-lg-9">
+    <?php get_template_part('layouts/ad', 'block'); ?>
+  </div>
   <?php while($more_query->have_posts()): $more_query->the_post(); ?>
-    <?php if($counter > 6) { $counter = 1; } ?>
+    <!-- ?php if($counter > 6) { $counter = 1; } ? -->
 
     <?php if ($counter == 1) : ?>
-      <div class="col-sm-12 col-md-4 push-md-8 col-lg-3 push-lg-9">
-        <?php get_template_part('layouts/ad', 'block'); ?>
-      </div>
-    <?php elseif($counter == 2): ?>
       <div class="col-sm-6 col-md-8 pull-md-4 col-lg-6 pull-lg-3">
         <?php get_template_part('layouts/card', 'card__2-1'); ?>
       </div>
-    <?php elseif($counter == 3): ?>
+    <?php elseif($counter == 2): ?>
       <div class="col-sm-6 col-md-8 pull-md-4 col-lg-3 pull-lg-3">
         <?php get_template_part('layouts/card', 'card__1-1'); ?>
       </div>
