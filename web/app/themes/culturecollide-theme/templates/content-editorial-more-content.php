5643e663e0a96ca2fb6bf7128813481f0fc158e9
pandabrand <pandabrand@gmail.com>
2017-12-10 13:05:24 -0600

    adjusting the more content filters

--- web/app/themes/culturecollide-theme/templates/content-editorial-more-content.php
+++ web/app/themes/culturecollide-theme/templates/content-editorial-more-content.php
@@ -26,6 +26,7 @@ if( is_page('editorial') ) {
 $reservedObj = get_category_by_slug('reserved');
 $exclude_cats[] = $reservedObj->term_id;
 
+// $paged = ( null == get_query_var( 'more_paged' ) || get_query_var( 'more_paged' ) == 0 ) ? '1' : get_query_var( 'more_paged' );
 if ( get_query_var( 'paged' ) ) { $paged = get_query_var( 'paged' ); }
 elseif ( get_query_var( 'page' ) ) { $paged = get_query_var( 'page' ); }
 else { $paged = 1; }
@@ -43,6 +44,15 @@ if( is_single() && !empty($post_tags) ) {
 }
 
 $more_query = new WP_Query($args);
+$has_more_posts = $more_query->have_posts();
+
+// if there are no more posts and paged is less than 11 change 'tag__in to empty
+if(is_single() && !$has_more_posts && $paged < 11) {
+  $args['tag__in'] = array();
+  $more_query = new WP_Query($args);
+  $has_more_posts = $more_query->have_posts();
+}
+
 if($more_query->have_posts()): $counter = 1; ?>
 <div class="row cc-row d-flex justify-content-center">
   <div class="editorial__title">
@@ -56,8 +66,6 @@ if($more_query->have_posts()): $counter = 1; ?>
     <?php get_template_part('layouts/ad', 'block'); ?>
   </div>
   <?php while($more_query->have_posts()): $more_query->the_post(); ?>
-    <!-- ?php if($counter > 6) { $counter = 1; } ? -->
-
     <?php if ($counter == 1) : ?>
       <div class="col-sm-6 col-md-8 pull-md-4 col-lg-6 pull-lg-3">
         <?php get_template_part('layouts/card', 'card__2-1'); ?>
@@ -67,20 +75,21 @@ if($more_query->have_posts()): $counter = 1; ?>
         <?php get_template_part('layouts/card', 'card__1-1'); ?>
       </div>
     <?php else: ?>
-      <div class="col-xs-12 col-sm-4">
+      <div class="infinite-item col-xs-12 col-sm-4">
         <?php get_template_part('layouts/card', 'card__3-1'); ?>
       </div>
   <?php endif; ?>
   <?php $counter++; endwhile; ?>
 </div>
-<?php endif; ?>
-<?php wp_reset_query(); ?>
-<?php
-  global $wp;
-  $current_url = home_url( $wp->query_vars['pagename'] );
+<?php 
+    $paged++;
+  endif;
+  wp_reset_query();
+  $current_url = home_url( $post->post_name );
+  write_log($paged);
 ?>
 <div class="row cc-row d-flex justify-content-center">
-    <?php if( $paged + 1 < 11 ): ?>
-      <a class="infinite-more-link button button--outline" onclick="javascript:(event.preventDefault())" href="<?php echo trailingslashit($current_url), $paged + 1; ?>">More <i class="fa fa-circle-o-notch fa-spin fa-3x fa-fw loader-anim"></i></a>
+    <?php if( $paged < 11 && $has_more_posts): ?>
+      <a class="infinite-more-link button button--outline" onclick="javascript:(event.preventDefault())" href="<?php echo trailingslashit($current_url), $paged; ?>">More <i class="fa fa-circle-o-notch fa-spin fa-3x fa-fw loader-anim"></i></a>
     <?php endif; ?>
 </div>
