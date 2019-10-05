d3d6c62d6a5a8676cf3d903de78f1ce7c68665c1
pandabrand <pandabrand@gmail.com>
2019-10-05 18:36:59 -0500

    remove travel from front

--- web/app/themes/culturecollide-theme/templates/content-front.php
+++ web/app/themes/culturecollide-theme/templates/content-front.php
@@ -1,6 +1,6 @@
 <?php get_template_part('templates/content', 'feature_block'); ?>
 <?php $category_post_type = 'culture'; ?>
 <?php include( locate_template( 'templates/content-related-content.php' ) ); ?>
-<?php $category_post_type = 'travel'; ?>
+<?php $category_post_type = 'culture'; ?>
 <?php include( locate_template( 'templates/content-related-content.php' ) ); ?>
 <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
