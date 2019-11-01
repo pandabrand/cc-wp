07330a48efdfc83d35d2188c70090dff7acbeb22
pandabrand <pandabrand@gmail.com>
2019-11-01 09:03:17 -0500

    new sidebars

--- web/app/themes/culturecollide-theme/lib/setup.php
+++ web/app/themes/culturecollide-theme/lib/setup.php
@@ -97,6 +97,22 @@ function widgets_init() {
     'before_title'  => '<h3>',
     'after_title'   => '</h3>'
   ]);
+  register_sidebar([
+    'name'          => __('Twitter', 'sage'),
+    'id'            => 'cc-twitter',
+    'before_widget' => '<section class="widget %1$s %2$s">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>'
+  ]);
+  register_sidebar([
+    'name'          => __('Instagram', 'sage'),
+    'id'            => 'cc-instagram',
+    'before_widget' => '<section class="widget %1$s %2$s">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>'
+  ]);
 }
 add_action('widgets_init', __NAMESPACE__ . '\\widgets_init');
 
